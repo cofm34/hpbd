53ed2ec174484634e8734869ba46379319f16db8
cofm dubaii <96689200+cofm34@users.noreply.github.com>
2025-03-24 16:15:39 +0800

    update

---
+++ config.js
@@ -0,0 +1,48 @@
+var config = {
+
+    texts: [
+        "Gửi đến",      // Đây, dấu phẩy cuối cùng của mỗi câu phải là dấu phẩy tiếng Anh!! Rất quan trọng!!
+        "Chung Hồng Yến",  // Tương tự...
+        "Hôm nay là sinh nhật của em",
+        "Đây là sinh nhật thứ 21",
+        "Chúc em sinh nhật vui vẻ",
+        "Cảm ơn em vì đã xuất hiện trong cuộc đời này",
+        "Làm cho mọi ngày của anh",
+        "Đều ngọt ngào như chiếc bánh sinh nhật hôm nay.",
+        "Chúc em luôn hạnh phúc",
+        "Mãi cười thật tươi",
+        "Và để anh...",
+        "Được nắm tay em",
+        "Đi qua mọi mùa sinh nhật sau này!",
+        "Chúc mừng sinh nhật em iuu~~"
+    ],
+
+    imgs: {
+        "Gửi đến": "./imgs/songxinxin.jpeg",
+        "Chung Hồng Yến": "./imgs/nothing.jpg",
+        "Hôm nay là sinh nhật của em": "./imgs/shengri.jpeg",
+        "Đây là sinh nhật thứ 21": "./imgs/zaiyiqi.png",
+        "Chúc em sinh nhật vui vẻ": "./imgs/nothing.jpg",
+        "Cảm ơn em vì đã xuất hiện trong cuộc đời này": "./imgs/yangzi.jpeg",
+        "Đều ngọt ngào như chiếc bánh sinh nhật hôm nay.": "./imgs/nothing.jpg",
+        "Chúc em luôn hạnh phúc": "./imgs/nothing.jpg",
+        "Mãi cười thật tươi": "./imgs/nothing.jpg",
+        "Và để anh...": "./imgs/nothing.jpg",
+        "Được nắm tay em": "./imgs/chuo.gif",
+        "Đi qua mọi mùa sinh nhật sau này!": "./imgs/chihaodian.jpeg",
+        "Chúc mừng sinh nhật em iuu~~": "./imgs/weibao.jpeg"
+    },
+    
+    desc: {
+        turn_on: "Nhấn vào đây để bắt đầu",
+        play: "Bật nhạc",
+        bannar_coming: "Thêm chút màu sắc",
+        balloons_flying: "Hình như còn thiếu gì đó",
+        cake_fadein: "Bánh kem?",
+        light_candle: "Nến?",
+        wish_message: "Chúc mừng sinh nhật～",
+        story: "Một thông điệp dành cho em"
+    },
+
+    loveText: 'Yêu Em~'
+}
